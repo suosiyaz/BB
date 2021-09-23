fbfe528b3099944eab4e9d87ba3a9c008b8ec97e
MOHAMMED SHIYAS P A <siyazputhancode@gmail.com>
2021-09-23 23:49:48 +0530

    Styling Client

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,19 +5,22 @@ import { RouterModule } from '@angular/router';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ToastrModule } from 'ngx-toastr';
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, ServerErrorComponent, NotFoundComponent],
+  declarations: [NavBarComponent, ServerErrorComponent, NotFoundComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
     RouterModule,
+    BreadcrumbModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
     })
   ],
-  exports: [NavBarComponent]
+  exports: [NavBarComponent, SectionHeaderComponent]
 })
 export class CoreModule { }
