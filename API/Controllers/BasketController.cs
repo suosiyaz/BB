5bd172f73c95ca004ed852ec26dad9f77131ef59
MOHAMMED SHIYAS P A <siyazputhancode@gmail.com>
2021-09-24 20:21:08 +0530

    API Validation

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -11,28 +13,31 @@ namespace API.Controllers
     public class BasketController : BaseApiController
     {
         private readonly IBasketRepository _basketRepository;
-        public BasketController(IBasketRepository basketRepository)
+        private readonly IMapper _mapper;
+        public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _basketRepository = basketRepository;
         }
 
         [HttpGet]
         public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
-        { 
+        {
             var basket = await _basketRepository.GetBasketAsync(id);
             return Ok(basket ?? new CustomerBasket(id));
         }
 
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
-        { 
-            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
+        {
+            var customerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
             return Ok(updatedBasket);
         }
 
         [HttpDelete]
         public async Task DeleteBasketAsync(string id)
-        { 
+        {
             await _basketRepository.DeleteBasketAsync(id);
         }
     }
