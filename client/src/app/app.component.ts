81880d5aef1658118f0a29bc6181d1c5f1fa6a7a
MOHAMMED SHIYAS P A <siyazputhancode@gmail.com>
2021-09-25 10:29:56 +0530

    Client Identity

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
   title = 'BilBo';
 
-  constructor(private basketService: BasketService) {}
+  constructor(
+    private basketService: BasketService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+
+    this.accountService.loadCurrentUser(token).subscribe(
+      () => {
+        console.log('loaded user');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
-      this.basketService.getBasket(basketId).subscribe(() => {
-        console.log('initialized basket');
-      }, error => {
-        console.log(error);
-      });
+      this.basketService.getBasket(basketId).subscribe(
+        () => {
+          console.log('initialized basket');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
   }
 }
